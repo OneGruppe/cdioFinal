04f3d41e2862962772be2f578d7fb0ff3622bf50
Sebastian <s170423@student.dtu.dk>
2018-06-02 14:32:29 +0200

    Connector class created

---
+++ src/data/Connector.java
@@ -0,0 +1,85 @@
+package data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import exceptions.DALException;
+
+public class Connector 
+{
+	/**
+	 * 
+	 * @param url to MySQL Database
+	 * @param username for MySQL user
+	 * @param password for MySQL user
+	 * @return a connection to the database URL
+	 * @throws SQLException
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 * @throws ClassNotFoundException
+	 */
+	public static Connection connectTo(String url, String username, String password) 
+	throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException
+	{
+		// call the driver class' no argument constructor
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+		
+		// get Connection-object via DriverManager
+		return (Connection) DriverManager.getConnection(url, username, password);
+	}
+	
+	private static Connection conn;
+	private static Statement stm;
+	
+	/**
+	 * 
+	 * @param server
+	 * @param port
+	 * @param database
+	 * @param username
+	 * @param password
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
+	public Connector(String server, int port, String database, String username, String password) 
+	throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException
+	{
+		conn	= connectTo("jdbc:mysql://"+server+":"+port+"/"+database, username, password);
+		stm		= conn.createStatement();
+	}
+	
+	/**
+	 * 
+	 * @param command (SQL query command)
+	 * @return a ResultSet object of data produced by the given query command
+	 * @throws DALException
+	 */
+	public ResultSet doQuery(String command) throws DALException
+	{
+		try {
+			return stm.executeQuery(command);
+		} catch (SQLException e) {
+			throw new DALException(e.getMessage());
+		}
+	}
+	
+	/**
+	 * 
+	 * @param command (SQL query command for updates)
+	 * @return an Interger. Either row count for SQL DML or 0 for SQL that returns nothing
+	 * @throws DALException
+	 */
+	public int doUpdate(String command) throws DALException
+	{
+		try {
+			return stm.executeUpdate(command);
+		} catch (SQLException e) {
+			throw new DALException(e.getMessage());
+		}
+	}
+}
