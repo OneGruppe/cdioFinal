85fb638e47e241dada17e162ec2ce6c4b16fc2fb
Sebastian <s170423@student.dtu.dk>
2018-06-02 09:50:42 +0200

    add appconfig

---
+++ src/AppConfig.java
@@ -0,0 +1,10 @@
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Sætter path for alle restkald
+ */
+@ApplicationPath("/rest")
+public class AppConfig extends Application {
+
+}
