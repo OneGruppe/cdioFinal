394149b4ea908e54fed8ae6f234158b19407a5a8
Mathias Fager <mathias_1337@hotmail.com>
2018-06-04 13:27:28 +0200

    JAVA: CommodityDAO finished but untested

--- src/data/dao_implementation/CommodityDAO.java
+++ src/data/dao_implementation/CommodityDAO.java
@@ -6,12 +6,14 @@ import exceptions.DALException;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import data.connector.*;
 
 public class CommodityDAO implements ICommodityDAO {
 	Connector conn;
-	
+
 	public CommodityDAO ()
 	{
 		try {
@@ -21,13 +23,13 @@ public class CommodityDAO implements ICommodityDAO {
 			e.printStackTrace();
 		}
 	}
-	
+
 
 	@Override
 	public void createCommodity(CommodityDTO commodity) throws DALException 
 	{
 		String commodityQuery = "CALL createCommodity (" + commodity.getCommodityID() + ", " + commodity.getCommodityName() + ", " + commodity.getSupplier() + ")";
-		
+
 		conn.doUpdate(commodityQuery);
 	}
 
@@ -35,7 +37,7 @@ public class CommodityDAO implements ICommodityDAO {
 	public void updateCommodity(CommodityDTO commodity) throws DALException 
 	{
 		String commodityQuery = "CALL updateCommodity(" + commodity.getCommodityID() + ", " + commodity.getCommodityName() + ", " + commodity.getSupplier() + ")";
-		
+
 		conn.doUpdate(commodityQuery);
 	}
 
@@ -43,7 +45,7 @@ public class CommodityDAO implements ICommodityDAO {
 	public void deleteCommodity(int commodityID) throws DALException 
 	{
 		String commodityQuery = "DELETE FROM commoity WHERE commodity ID = " + commodityID + "";
-		
+
 		conn.doUpdate(commodityQuery);
 	}
 
@@ -51,14 +53,56 @@ public class CommodityDAO implements ICommodityDAO {
 	public CommodityDTO showCommodity(int commodityID) throws DALException
 	{
 		String commodityQuery = "SELECT * FROM commodityView WHERE commodityID = " + commodityID + "";
-		
-		ResultSet rsName = conn.doQuery(commodityQuery);
-	}
-	
-	@Override
-	public void showAllCommodities() throws DALException
-	{
-		
+
+		int id = 0;
+		String commodityName = null;
+		String supplierName = null;
+
+		ResultSet rs = conn.doQuery(commodityQuery);
+		try
+		{
+			while(rs.next())
+			{
+				id = rs.getInt("commodityID");
+				commodityName = rs.getString("commodityName");
+				supplierName = rs.getString("supplierName");
+			}
+			CommodityDTO commodity = new CommodityDTO(id, commodityName, supplierName);
+			return commodity;
+		} catch (SQLException e) {
+			throw new DALException("SQLException in showCommodity(): " + e.getMessage());
+		}
 	}
 
+	@Override
+	public List<CommodityDTO> showAllCommodities() throws DALException
+	{
+		List<CommodityDTO> commodityList = new ArrayList<CommodityDTO>();
+
+		String commodityQuery = "SELECT * FROM commodityView";
+
+		int id = 0;
+		String commodityName = null;
+		String supplierName = null;
+
+		ResultSet rs = conn.doQuery(commodityQuery);
+
+		try
+		{
+			while(rs.next())
+			{
+				id = rs.getInt("commodityID");
+				commodityName = rs.getString("commodityName");
+				supplierName = rs.getString("supplierName");
+
+				CommodityDTO commodity = new CommodityDTO(id, commodityName, supplierName);
+				commodityList.add(commodity);
+
+			}
+			return commodityList;
+		} catch (SQLException e) {
+			throw new DALException("SQLException in showAllCommodities(): " + e.getMessage());
+		}
+
+	}
 }
