e97cc99e6c1996781ca5239a5d4641f658d5434f
Sebastian <s170423@student.dtu.dk>
2018-06-04 09:51:37 +0200

    dao_implementation init

---
+++ src/data/dao_implementation/CommodityBatchDAO.java
@@ -0,0 +1,27 @@
+package data.dao_implementation;
+
+import data.dao_interface.ICommodityBatchDAO;
+import data.dto.CommodityBatchDTO;
+import exceptions.DALException;
+
+public class CommodityBatchDAO implements ICommodityBatchDAO {
+
+	@Override
+	public void createCommodityBatch(CommodityBatchDTO commodityBatch) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateCommodityBatch(CommodityBatchDTO commodityBatch) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteCommodityBatch(CommodityBatchDTO commodityBatch) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
