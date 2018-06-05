135a67c4b3401fcd623ba8f8d11e56d569c44f44
Niklaes Jacobsen <niklaes@gmail.com>
2018-06-05 17:11:43 +0200

    JAVA: ProductBatchDAOTest - create productbatch done

--- src/test/dao/ProductBatchDAOTest.java
+++ src/test/dao/ProductBatchDAOTest.java
@@ -3,15 +3,25 @@ package test.dao;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import data.dao_implementation.CommodityBatchDAO;
 import data.dao_implementation.CommodityDAO;
 import data.dao_implementation.ProductBatchDAO;
 import data.dao_implementation.RecipeDAO;
+import data.dao_implementation.SupplierDAO;
+import data.dao_implementation.UserDAO;
+import data.dto.CommodityBatchDTO;
+import data.dto.CommodityDTO;
 import data.dto.ProductBatchDTO;
 import data.dto.RecipeDTO;
+import data.dto.SupplierDTO;
+import data.dto.UserDTO;
 import exceptions.DALException;
 
 public class ProductBatchDAOTest
@@ -19,6 +29,9 @@ public class ProductBatchDAOTest
 	ProductBatchDAO productbdao;
 	RecipeDAO recipedao;
 	CommodityDAO commoditydao;
+	SupplierDAO supplierdao;
+	UserDAO userdao;
+	CommodityBatchDAO commoditybatchdao;
 
 	@Before
 	public void connect()
@@ -26,8 +39,11 @@ public class ProductBatchDAOTest
 		try
 		{
 			commoditydao = new CommodityDAO();
+			commoditybatchdao = new CommodityBatchDAO();
 			productbdao = new ProductBatchDAO();
 			recipedao = new RecipeDAO();
+			supplierdao = new SupplierDAO();
+			userdao = new UserDAO();
 		}
 		catch(DALException e)
 		{
@@ -49,15 +65,44 @@ public class ProductBatchDAOTest
 	}
 
 	@Test
-	public void createAndGetUser() 
+	public void createProductBatch() 
 	{
-		RecipeDTO recipe = new RecipeDTO(5, "Pizza", commodityID, nomNetto, recipeTolerance)
-				try
+		try
 		{
-					ProductBatchDTO expected = new ProductBatchDTO(3, 1, recipeID, userID, comBatID, tara, netto);
-					productbdao.createProductBatch(expected);
-					ProductBatchDTO actual = productbdao.getProductBatch(3);
-					assertEquals(expected.toString(), actual.toString());
+			//Make a supplier
+			SupplierDTO suppdto = new SupplierDTO(6, "Simons Lager");
+			supplierdao.createSupplier(suppdto);
+
+			//Make a supplierList
+			List<SupplierDTO> suppList = new ArrayList<SupplierDTO>();
+			suppList.add(suppdto);
+
+			//Make a commodity
+			CommodityDTO comdto = new CommodityDTO(1, "Tomat", suppList);
+			commoditydao.createCommodity(comdto);
+
+			//Make commodityBatch
+			CommodityBatchDTO combatdto = new CommodityBatchDTO(4, comdto.getCommodityID(), 20);
+			commoditybatchdao.createCommodityBatch(combatdto);
+
+			//Make a commodityList
+			List<Integer> comList = new ArrayList<Integer>();
+			comList.add(comdto.getCommodityID());
+
+			//Make a recipe
+			RecipeDTO recipedto = new RecipeDTO(5, "Pizza", comList, 0.5, 0.1);
+			recipedao.createRecipe(recipedto);
+
+			//Make a user
+			UserDTO userdto = new UserDTO(3, "Thomas Nielsen", "TN", 1);
+			userdao.createUser(userdto);
+
+			//Make a productBatch
+			ProductBatchDTO expected = new ProductBatchDTO(3, 1, recipedto.getRecipeID(), userdto.getUserID(), combatdto.getCbid(), 0.1, 10);
+			productbdao.createProductBatch(expected);
+			
+			ProductBatchDTO actual = productbdao.getProductBatch(3);
+			assertEquals(expected.toString(), actual.toString());
 		}
 		catch(DALException e)
 		{
@@ -65,7 +110,8 @@ public class ProductBatchDAOTest
 		}
 	}
 
-	@Test
+	/*
+	@Ignore
 	public void updateAndGetUser() 
 	{
 		ProductBatchDTO exPBatch = new ProductBatchDTO(3, 1, recipeID, userID, comBatID, tara, netto);
@@ -87,15 +133,13 @@ public class ProductBatchDAOTest
 		{
 			fail("Error " + e.getMessage());
 		}
-		 */
 	}
 
-	@Test
+	@Ignore
 	public void getALLUser() 
 	{
 		ProductBatchDTO userPBatch = new ProductBatchDTO(3, status, recipeID, userID, comBatID, tara, netto);
 
-		/*
 		try
 		{
 			userdao.createUser(userOne);
@@ -112,7 +156,7 @@ public class ProductBatchDAOTest
 		{
 			fail("Error " + e.getMessage());
 		}
-		 */
 	}
+	*/
 
 }
