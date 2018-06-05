a7bc43c0d0c5371cda57f76d33ed22db1550071a
Mathias Fager <mathias_1337@hotmail.com>
2018-06-05 11:54:08 +0200

    JAVA: supplierDAO done

---
+++ src/data/dao_implementation/SupplierDAO.java
@@ -0,0 +1,112 @@
+package data.dao_implementation;
+
+import data.connector.Connector;
+import data.dao_interface.ISupplierDAO;
+import data.dto.SupplierDTO;
+import exceptions.DALException;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SupplierDAO implements ISupplierDAO
+{
+	Connector con;
+	
+	public SupplierDAO()throws DALException
+	{
+		try 
+		{
+			con = new Connector();
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) 
+		{
+			throw new DALException(e.getMessage());
+		}
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see data.dao_interface.ISupplierDAO#createSupplier(data.dto.SupplierDTO)
+	 */
+	@Override
+	public void createSupplier(SupplierDTO supplier) throws DALException
+	{
+		con.doUpdate("INSERT INTO supplier VALUES (" + supplier.getSupID() + ", '" + supplier.getSupName() + "'");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see data.dao_interface.ISupplierDAO#updateSupplier(data.dto.SupplierDTO)
+	 */
+	@Override
+	public void updateSupplier(SupplierDTO supplier) throws DALException {
+		con.doUpdate("UPDATE supplier WHERE supplierID = '" + supplier.getSupID() + "' SET (supplierName = '" + supplier.getSupName() + "')");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see data.dao_interface.ISupplierDAO#deleteSupplier(int)
+	 */
+	@Override
+	public void deleteSupplier(int supplierID) throws DALException {
+		con.doUpdate("DELETE supplier WHERE 'supplierID' = '" + supplierID + "'");		
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see data.dao_interface.ISupplierDAO#showSupplier(int)
+	 */
+	@Override
+	public SupplierDTO showSupplier(int supplierID) throws DALException {
+		String supplierName = null;
+		
+		ResultSet rs = con.doQuery("SELECT * FROM supplier WHERE 'supplierID' = '" + supplierID + "'");
+		try
+		{
+			if(!rs.first())
+				throw new DALException();
+			while(rs.next())
+			{
+				supplierName = rs.getString("supplierName");
+			}
+		} catch (SQLException e)
+		{
+			throw new DALException(e.getMessage());
+		}
+		return new SupplierDTO(supplierID, supplierName);
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see data.dao_interface.ISupplierDAO#showAllSuppliers()
+	 */
+	@Override
+	public List<SupplierDTO> showAllSuppliers() throws DALException {
+		int suppID = 0;
+		String supplierName = null;
+		
+		List<SupplierDTO> suppList = null;
+		ResultSet rs = con.doQuery("SELECT * FROM supplier");
+		
+		try
+		{
+			if(!rs.first())
+			{
+				throw new DALException();
+			}
+			while(rs.next())
+			{
+				suppID = rs.getInt("supplierID");
+				supplierName = rs.getString("supplierName");
+				
+				suppList.add(new SupplierDTO(suppID, supplierName));
+			}
+		} catch (SQLException e)
+		{
+			throw new DALException(e.getMessage());
+		}
+		return suppList;
+	}
+}
