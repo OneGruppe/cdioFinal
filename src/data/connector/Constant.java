7579539240b213a8d97d15e4be79e1bf941b90ef
Niklaes Jacobsen <niklaes@gmail.com>
2018-06-05 23:57:32 +0200

    JAVA: protect Constant Class

--- src/data/connector/Constant.java
+++ src/data/connector/Constant.java
@@ -3,12 +3,12 @@ package data.connector;
 public class Constant
 {
 	
-	public static final String
+	protected static final String
 	server					= "91.100.3.26", 
 	database				=  "CDIOFinal",
 	username				= "Eclipse-bruger", 
 	password				= "ySmTL37uDjYZmzyn";
 
-	public static final int
+	protected static final int
 	port					= 9865;
 }
