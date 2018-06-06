a17b73fe9ac4ee6b483c796ab42b13149eb7b9af
Mathias Fager <mathias_1337@hotmail.com>
2018-06-06 13:46:38 +0200

    JAVA: flowcontrol with while loop and breaks

--- src/controller/controller_implementation/FlowController.java
+++ src/controller/controller_implementation/FlowController.java
@@ -17,68 +17,77 @@ public class FlowController
 
 		ProductBatchController pbc = new ProductBatchController();
 
-		switch(state)
+		while(true)
 		{
-		case 1:
-		{
-			weight.removeMsg();
-			weight.getInputWithMsg("Tryk temp for at fortsaette, temp2 for at gaa tilbage");
-			state +=1;
-		}
-		case 2:
-		{
-			System.out.println("-----------------------");
-			System.out.println("TRIES TO GET USERID");
-			userID = weight.getInputWithMsg("Indtast operatoer nummer");
-			System.out.println("USER ID: " + userID);
-			System.out.println("-----------------------");
-
-			if(userID != temp2)
+			switch(state)
 			{
-				state += 1;
+			case 1:
+			{
+				weight.removeMsg();
+				weight.getInputWithMsg("Tryk temp for at fortsaette, temp2 for at gaa tilbage");
+				state +=1;
+				break;
 			}
-			else state -= 1;
-		}
-		case 3:
-		{
-			int choice = weight.getInputWithMsg("Velkommen " + user.getUser(userID).getName()); 
+			case 2:
+			{
+				System.out.println("-----------------------");
+				System.out.println("TRIES TO GET USERID");
+				userID = weight.getInputWithMsg("Indtast operatoer nummer");
+				System.out.println("USER ID: " + userID);
+				System.out.println("-----------------------");
 
-			System.out.println("-----------------------");
-			System.out.println("Welcome: " + user.getUser(userID).getName());
-			System.out.println("-----------------------");
+				if(userID != temp2)
+				{
+					state += 1;
+				}
+				else state -= 1;
+				break;
+			}
+			case 3:
+			{
+				int choice = weight.getInputWithMsg("Velkommen " + user.getUser(userID).getName()); 
 
-			if(choice = temp)
-				state += 1;
-			else state -= 1;
-		}
-		case 4:
-		{
-			System.out.println("-----------------------");
-			System.out.println("GETS PRODUCTBATCHID");
-			productBatchID = weight.getInputWithMsg("Indtast productBatchID");
-			System.out.println("PRODUCTBATCH ID = " + productBatchID);
-			System.out.println("-----------------------");
+				System.out.println("-----------------------");
+				System.out.println("Welcome: " + user.getUser(userID).getName());
+				System.out.println("-----------------------");
 
-			if (productBatchID != temp2)
-				state += 1;
-			else state -= 1;
-		}
-		case 5:
-		{
-			System.out.println("-----------------------");
-			System.out.println("GETS TARA");
-			tara = weight.getInputWithMsg("Placer beholder på vaegt");
-			System.out.println("TARA = " + tara);
-			System.out.println("-----------------------");
+				if(choice = temp)
+					state += 1;
+				else state -= 1;
+				break;
+			}
+			case 4:
+			{
+				System.out.println("-----------------------");
+				System.out.println("GETS PRODUCTBATCHID");
+				productBatchID = weight.getInputWithMsg("Indtast productBatchID");
+				System.out.println("PRODUCTBATCH ID = " + productBatchID);
+				System.out.println("-----------------------");
 
-			if(tara != temp2)
-				state += 1;
-			else state -= 1;
-		}
-		case 6:
-		{
-			weight.getInputWithMsg("Vej: " + pbc.getProductBatch(productBatchID));
-		}
+				if (productBatchID != temp2)
+					state += 1;
+				else state -= 1;
+				break;
+			}
+			case 5:
+			{
+				System.out.println("-----------------------");
+				System.out.println("GETS TARA");
+				tara = weight.getInputWithMsg("Placer beholder på vaegt");
+				System.out.println("TARA = " + tara);
+				System.out.println("-----------------------");
+
+				if(tara != temp2)
+					state += 1;
+				else state -= 1;
+				break;
+			}
+			case 6:
+			{
+				weight.getInputWithMsg("Vej: " + pbc.getProductBatch(productBatchID));
+			}
+			}
+
 		}
 	}
 }
