f9883335aaad660861553e2166f3f4f2380f9f19
Sebastian <s170423@student.dtu.dk>
2018-06-07 15:48:35 +0200

    WEB: Show user fungerer på siden

--- WebContent/js/showUser.js
+++ WebContent/js/showUser.js
@@ -14,6 +14,83 @@ $(document).ready(function() {
 			success:function(data) {
 				alert(data);
 				console.log(data.ini);
+				
+				if(document.contains(document.getElementById("showUserTable")))
+				{
+					document.getElementById("showUserTable").remove();
+					var t, r, c;
+					
+					t = document.createElement("table");
+					t.setAttribute("id", "showUserTable");
+					
+					r = t.insertRow(0);
+					
+					c = r.insertCell(0);
+					c.innerHTML = "ID";
+					
+					c = r.insertCell(1);
+					c.innerHTML = "Navn";
+					
+					c = r.insertCell(2);
+					c.innerHTML = "Initialer";
+					
+					c = r.insertCell(3);
+					c.innerHTML = "Status";
+					
+					r = t.insertRow(1);
+					
+					c = r.insertCell(0);
+					c.innerHTML = data.ID;
+					
+					c = r.insertCell(1);
+					c.innerHTML = data.name;
+					
+					c = r.insertCell(2);
+					c.innerHTML = data.ini;
+					
+					c = r.insertCell(3);
+					c.innerHTML = data.active;
+					
+					document.getElementById("showUser").appendChild(t);
+						
+				} 
+				else
+				{
+					var t, r, c;
+					
+					t = document.createElement("table");
+					t.setAttribute("id", "showUserTable");
+					
+					r = t.insertRow(0);
+					
+					c = r.insertCell(0);
+					c.innerHTML = "ID";
+					
+					c = r.insertCell(1);
+					c.innerHTML = "Navn";
+					
+					c = r.insertCell(2);
+					c.innerHTML = "Initialer";
+					
+					c = r.insertCell(3);
+					c.innerHTML = "Status";
+					
+					r = t.insertRow(1);
+					
+					c = r.insertCell(0);
+					c.innerHTML = data.ID;
+					
+					c = r.insertCell(1);
+					c.innerHTML = data.name;
+					
+					c = r.insertCell(2);
+					c.innerHTML = data.ini;
+					
+					c = r.insertCell(3);
+					c.innerHTML = data.active;
+					
+					document.getElementById("showUser").appendChild(t);
+				}
 			}
 		});
 		return false;
