a547ee331bbf87d109288b3bb4e539b3e5bb6ec1
Sebastian <s170423@student.dtu.dk>
2018-06-07 13:31:03 +0200

    WEB: Admin/Prod fixes

--- WebContent/js/navigation.js
+++ WebContent/js/navigation.js
@@ -7,12 +7,20 @@ $(document).ready(function() {
 	$('#findUserForm').hide();
 	
 	$('#navigateToCreateUser').click(function() {
+		$('#showUsersTable').fadeOut(200);
 		$('#findUserForm').fadeOut(200);
 		$('#createUserForm').delay(300).fadeIn(200);
 	})
 	
 	$('#navigateToFindUser').click(function() {
+		$('#showUsersTable').fadeOut(200);
 		$('#createUserForm').fadeOut(200);
 		$('#findUserForm').delay(300).fadeIn(200);
+	})
+	
+	$('#navigateToShowUsers').click(function() {
+		$('#createUserForm').fadeOut(200);
+		$('#findUserForm').fadeOut(200);
+		$('#showUsersTable').delay(300).fadeIn(200);
 	})
 })
