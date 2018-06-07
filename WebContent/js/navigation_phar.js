2ae59e53dc93bdb50af8030327bd54439353d24b
Burhan <s175446@student.dtu.dk>
2018-06-07 11:19:00 +0200

    WEB: Pharmacist navigation complete

--- WebContent/js/navigation_phar.js
+++ WebContent/js/navigation_phar.js
@@ -37,15 +37,27 @@ $(document).ready(function() {
 	
 	
 	$('#navigateToFindCommodity').click(function() {
-		$('#createUserForm').fadeOut(200);
-		$('#findUserForm').delay(300).fadeIn(200);
+		$('#createCommodityForm').fadeOut(200);
+		$('#createCommodityBatchForm').fadeOut(200);
+		$('#createSupplierForm').fadeOut(200);
+		$('#findCommodityBatchForm').fadeOut(200);
+		$('#findSupplierForm').fadeOut(200);
+		$('#findCommodityForm').delay(300).fadeIn(200);
 	})
 	$('#navigateToFindCommodityBatch').click(function() {
-		$('#createUserForm').fadeOut(200);
-		$('#findUserForm').delay(300).fadeIn(200);
+		$('#createCommodityForm').fadeOut(200);
+		$('#createCommodityBatchForm').fadeOut(200);
+		$('#createSupplierForm').fadeOut(200);
+		$('#findCommodityForm').fadeOut(200);
+		$('#findSupplierForm').fadeOut(200);
+		$('#findCommodityBatchForm').delay(300).fadeIn(200);
 	})
 	$('#navigateToFindSupplier').click(function() {
-		$('#createUserForm').fadeOut(200);
-		$('#findUserForm').delay(300).fadeIn(200);
+		$('#createCommodityForm').fadeOut(200);
+		$('#createCommodityBatchForm').fadeOut(200);
+		$('#createSupplierForm').fadeOut(200);
+		$('#findCommodityForm').fadeOut(200);
+		$('#findCommodityBatchForm').fadeOut(200);
+		$('#findSupplierForm').delay(300).fadeIn(200);
 	})
 })
