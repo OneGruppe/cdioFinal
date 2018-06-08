170d98e7539a9eafe3a4e147ca96af7a7cf977c8
Burhan <s175446@student.dtu.dk>
2018-06-08 14:15:58 +0200

    REST: Minor fixes

--- src/boundary/rest_implementation/RecipeREST.java
+++ src/boundary/rest_implementation/RecipeREST.java
@@ -1,6 +1,15 @@
 package boundary.rest_implementation;
 
+
 import java.util.List;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import boundary.rest_interface.IRecipeREST;
 import controller.controller_implementation.RecipeController;
@@ -11,29 +20,46 @@ public class RecipeREST implements IRecipeREST {
 	
 	private RecipeController rc;
 	
-	public RecipeREST() throws DALException {
-		rc = new RecipeController();
+	public RecipeREST()  
+	{	
+		try {
+			
+			rc = new RecipeController();
+			
+		} catch (DALException e) {
+			
+			System.out.println(e.getMessage());
+		}
 	}
 
 	@Override
+	@PUT
+	@Path("createRecipe")
 	public void createRecipe(int id, String name) throws DALException 
 	{
 		rc.createRecipe(id, name);
 	}
 
 	@Override
+	@POST
+	@Path("updateRecipe")
 	public void updateRecipe(int id, String name) throws DALException 
 	{
 		rc.updateRecipe(id, name);
 	}
 
 	@Override
+	@DELETE
+	@Path("deleteRecipe")
 	public void deleteRecipe(int id) throws DALException
 	{
 		rc.deleteRecipe(id);
 	}
 
 	@Override
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("getRecipe")
 	public RecipeDTO getRecipe(int id) throws DALException 
 	{
 		RecipeDTO rec;
@@ -42,6 +68,9 @@ public class RecipeREST implements IRecipeREST {
 	}
 
 	@Override
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("getAllRecipes")
 	public List<RecipeDTO> getAllRecipe() throws DALException 
 	{
 		List<RecipeDTO> recs;
--- src/boundary/rest_implementation/SupplierREST.java
+++ src/boundary/rest_implementation/SupplierREST.java
@@ -2,6 +2,14 @@ package boundary.rest_implementation;
 
 import java.util.List;
 
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 import boundary.rest_interface.ISupplierREST;
 import controller.controller_implementation.SupplierController;
 import data.dto.SupplierDTO;
@@ -12,30 +20,46 @@ public class SupplierREST implements ISupplierREST{
 	
 	private SupplierController sc;
 	
-	public SupplierREST() throws DALException 
+	public SupplierREST()
 	{
-		sc = new SupplierController();
+		try {
+			
+			sc = new SupplierController();
+			
+		} catch (DALException e) {
+			
+			System.out.println(e.getMessage());
+		}
 	}
 
 	@Override
+	@PUT
+	@Path("createSupplier")
 	public void createSupplier(int id, String name) throws DALException 
 	{
 		sc.createSupplier(id, name);	
 	}
 
 	@Override
+	@POST
+	@Path("updateSupplier")
 	public void updateSupplier(int id, String name) throws DALException 
 	{
 		sc.updateSupplier(id, name);
 	}
 
 	@Override
+	@DELETE
+	@Path("deleteSupplier")
 	public void deleteSupplier(int id) throws DALException 
 	{
 		sc.deleteSupplier(id);
 	}
 
 	@Override
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("getSupplier")
 	public SupplierDTO getSupplier(int id) throws DALException 
 	{	
 		SupplierDTO sup;
@@ -44,6 +68,9 @@ public class SupplierREST implements ISupplierREST{
 	}
 
 	@Override
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("getAllSuppliers")
 	public List<SupplierDTO> getAllSupplier() throws DALException 
 	{
 		List<SupplierDTO> sups;
