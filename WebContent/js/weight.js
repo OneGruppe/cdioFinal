f97ffd59b8c961142a5be128c285be9899466c3e
Unknown <s175195@student.dtu.dk>
2018-06-12 17:55:23 +0200

    Igæng js vægt

--- WebContent/js/weight.js
+++ WebContent/js/weight.js
@@ -2,19 +2,17 @@
  * weight.js
  */
 
-$(document).ready(function() {
 
-	$("#connectToWeight").click(function() {
-		$.ajax({
-			url: "/rest/weight/doConnection",
-			data: $('#').serialize(),
-			contenttype: "application/x-ww-form-urlencoded",
-			method: "POST",
-			success: function(data) {
-				console.log(data);
-				alert(data);
-			}
-		});
-		return false;
+$("#connectToWeight").click(function() {
+	$.ajax({
+		url: "/rest/weight/doConnection",
+		data: $('#').serialize(),
+		contenttype: "application/x-ww-form-urlencoded",
+		method: "POST",
+		success: function(data) {
+			console.log(data);
+			alert(data);
+		}
 	});
+	return false;
 });
