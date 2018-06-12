7042bb7cfb8f47a18878629f31fdf7f34ff48191
Mathias Fager <mathias_1337@hotmail.com>
2018-06-12 11:20:51 +0200

    JAVA: early alpha version of pre-alpha

--- src/test/TestWeightFlow.java
+++ src/test/TestWeightFlow.java
@@ -2,6 +2,7 @@ package test;
 
 import boundary.weight.WeightTranslation;
 import controller.controller_implementation.CommodityBatchController;
+import controller.controller_implementation.CommodityController;
 import controller.controller_implementation.ProductBatchController;
 import controller.controller_implementation.RecipeComponentController;
 import controller.controller_implementation.UserController;
@@ -18,9 +19,10 @@ public class TestWeightFlow {
 			UserController user = new UserController();
 			RecipeComponentController rcc = new RecipeComponentController();
 			CommodityBatchController cbc = new CommodityBatchController();
+			CommodityController cc = new CommodityController();
 			WeightTranslation weight = new WeightTranslation("62.79.16.17", 8000);
 
-			WeightController wc = new WeightController(pbc, user, rcc, cbc, weight);
+			WeightController wc = new WeightController(pbc, user, rcc, cbc, cc, weight);
 			wc.weightFlow();
 
 		} 
