8d502265c8e483bbf77ebb245a09ceb035e65d59
Unknown <s175195@student.dtu.dk>
2018-06-12 18:07:43 +0200

    seb was the BADman

--- WebContent/js/weight.js
+++ WebContent/js/weight.js
@@ -2,17 +2,19 @@
  * weight.js
  */
 
+$(document).ready(function() {
 
-$("#connectToWeight").click(function() {
-	$.ajax({
-		url: "/rest/weight/doConnection",
-		data: $('#').serialize(),
-		contenttype: "application/x-ww-form-urlencoded",
-		method: "POST",
-		success: function(data) {
-			console.log(data);
-			alert(data);
-		}
+	$("#connectToWeight").click(function() {
+		$.ajax({
+			url: "/rest/weight/doConnection",
+			/** data: $('#').serialize(), */
+			contenttype: "application/x-ww-form-urlencoded",
+			method: "POST",
+			success: function(data) {
+				console.log(data);
+				alert(data);
+			}
+		});
+		return false;
 	});
-	return false;
 });
