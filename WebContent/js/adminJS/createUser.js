459de33104d8a0ea20ea3e85ec5f5c8fa9e25e2a
Niklaes Jacobsen <niklaes@gmail.com>
2018-06-12 15:55:29 +0200

    JS: Create User test

--- WebContent/js/adminJS/createUser.js
+++ WebContent/js/adminJS/createUser.js
@@ -3,13 +3,16 @@
  */
 
 $(document).ready(function() {
-	
+
 	$("#creatingUser").click(function() {
 		$.ajax({
 			url: "/rest/user/createUser",
 			data: $('#createUserForm').serialize(),
 			contenttype: "application/x-ww-form-urlencoded",
 			method: "POST",
+			error: function(data) {
+				console.log("rofl");
+			},
 			success: function(data) {
 				alert("Brugeren er oprettet");
 			}
