b69ccb3aa6d7c68a9f6f75a0de9b5a82fa7339fa
Burhan <s175446@student.dtu.dk>
2018-06-13 15:04:46 +0200

    WEB: Show Recipe

---
+++ WebContent/js/pharmacistJS/showRecipe.js
@@ -0,0 +1,91 @@
+/**
+ * 
+ */
+
+$(document).ready(function() {
+	$("#findRecipe").click(function() {
+			
+		$.ajax({
+			url:"/rest/recipe/getRecipe",
+			data: $('#findRecipeForm').serialize(),
+			dataType: "json",
+			contenttype: "application/json",
+			method: "POST",
+			success:function(data) {
+				
+				console.log(data);
+				
+				if(data.id != undefined)
+				{
+					if(document.contains(document.getElementById("showRecTable")))
+					{
+						document.getElementById("showRecTable").remove();
+						var t, r, c;
+						
+						t = document.createElement("table");
+						t.setAttribute("id", "showRecTable");
+						
+						r = t.insertRow(0);
+						
+						c = r.insertCell(0);
+						c.innerHTML = "Recept ID";
+						
+						c = r.insertCell(1);
+						c.innerHTML = "Recept navn";
+						
+						
+						r = t.insertRow(1);
+						
+						c = r.insertCell(0);
+						c.innerHTML = data.id;
+						
+						c = r.insertCell(1);
+						c.innerHTML = data.name;
+						
+						c = r.insertCell(2);
+						c.innerHTML = data.supplierID;
+
+						
+						document.getElementById("showRecipe").appendChild(t);
+							
+					} 
+					else
+					{
+						
+						var t, r, c;
+						
+						t = document.createElement("table");
+						t.setAttribute("id", "showRecTable");
+						
+						r = t.insertRow(0);
+						
+						c = r.insertCell(0);
+						c.innerHTML = "Recept ID";
+						
+						c = r.insertCell(1);
+						c.innerHTML = "Recept navn";
+						
+						
+						r = t.insertRow(1);
+						
+						c = r.insertCell(0);
+						c.innerHTML = data.id;
+						
+						c = r.insertCell(1);
+						c.innerHTML = data.name;
+
+						
+						document.getElementById("showRecipe").appendChild(t);
+					}
+				}
+				else
+				{
+					alert("Ugyldigt ID blev indtastet")
+				}
+		
+			}
+				
+		});
+		return false;
+	})
+})
