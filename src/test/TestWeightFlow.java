7ced74adacb9bee596c832db46a9a1c23fed948d
Unknown <s175195@student.dtu.dk>
2018-06-13 14:10:23 +0200

    WeightFlow worki work

--- src/test/TestWeightFlow.java
+++ src/test/TestWeightFlow.java
@@ -16,7 +16,6 @@ public class TestWeightFlow {
 	{
 		try 
 		{
-			UserController uc = new UserController();
 			ProductBatchController pbc = new ProductBatchController();
 			UserController user = new UserController();
 			RecipeComponentController rcc = new RecipeComponentController();
@@ -25,7 +24,7 @@ public class TestWeightFlow {
 			ProductBatchComponentController pbcc = new ProductBatchComponentController();
 			WeightTranslation weight = new WeightTranslation("62.79.16.17", 8000);
 
-			WeightController wc = new WeightController(uc, pbc, user, rcc, cbc, cc, weight, pbcc);
+			WeightController wc = new WeightController(pbc, user, rcc, cbc, cc, weight, pbcc);
 			wc.weightFlow();
 
 		} 
