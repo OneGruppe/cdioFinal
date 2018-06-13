d525edc0146b05d254f06acb9903a7eb96eb6501
Niklaes Jacobsen <niklaes@gmail.com>
2018-06-13 19:31:14 +0200

    JAVA: WeightREST + IWeightREST
    klar til at få indput

--- src/boundary/rest/WeightREST.java
+++ src/boundary/rest/WeightREST.java
@@ -1,10 +1,14 @@
 package boundary.rest;
 
+import java.io.IOException;
+
+import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 import boundary.rest_interface.IWeightREST;
 import boundary.weight.WeightTranslation;
+import boundary.weight_interface.IWeightTranslation;
 import controller.controller.CommodityBatchController;
 import controller.controller.CommodityController;
 import controller.controller.ProductBatchComponentController;
@@ -12,31 +16,65 @@ import controller.controller.ProductBatchController;
 import controller.controller.RecipeComponentController;
 import controller.controller.UserController;
 import controller.controller.WeightController;
+import controller.controller_interface.ICommodityBatchController;
+import controller.controller_interface.ICommodityController;
+import controller.controller_interface.IProductBatchComponentController;
+import controller.controller_interface.IProductBatchController;
+import controller.controller_interface.IRecipeComponentController;
+import controller.controller_interface.IUserController;
 import controller.controller_interface.IWeightController;
+import data.connector.Constant;
 import exceptions.DALException;
+import exceptions.WeightException;
 
 @Path("Weight")
 public class WeightREST implements IWeightREST {
 
 	private IWeightController wc;
+	private IProductBatchController pbc;
+	private IUserController uc;
+	private IRecipeComponentController rcc;
+	private ICommodityBatchController cbc;
+	private ICommodityController cc;
+	private IProductBatchComponentController pbcc;
+	private IWeightTranslation iwc;
 
 	@Override
 	@POST
 	@Path("doConnection")
-	public String doConnection() throws DALException
+	public String doConnection(@FormParam("chooseWeightPort") String chooseWeightPort)
 	{
+		int weightPort = 0;
+
+		if(chooseWeightPort.equals("VÊgt 1"))
+		{
+			weightPort = Constant.weightPortOne;
+		}
+		else if (chooseWeightPort.equals("VÊgt 2"))
+		{
+			weightPort = Constant.weightPortTwo;
+		}
+		else
+		{
+			return "Der findes ikke en port til '" + chooseWeightPort + "'";
+		}
+
 		try 
 		{
-			wc = new WeightController(
-					new ProductBatchController(), new UserController(), 
-					new RecipeComponentController(), new CommodityBatchController(), 
-					new CommodityController(), new WeightTranslation("62.79.16.17", 8000), new ProductBatchComponentController());
-					wc.weightFlow();
+			pbc = new ProductBatchController();
+			uc = new UserController();
+			rcc = new RecipeComponentController();
+			cbc = new CommodityBatchController();
+			cc = new CommodityController();
+			pbcc = new ProductBatchComponentController();
+			iwc = new WeightTranslation(weightPort);
+			wc = new WeightController(pbc, uc, rcc, cbc, cc, pbcc, iwc);
+			wc.weightFlow();
 			return "Forbindelse blev oprettet korrekt";
 		} 
-		catch (DALException e) 
+		catch (DALException | IOException | WeightException e) 
 		{
-			return "Der skete en fejl, pr√∏v igen: " + e.getMessage();
+			return "Error: " + e.getMessage();
 		}
 	}
 
--- src/boundary/rest_interface/IWeightREST.java
+++ src/boundary/rest_interface/IWeightREST.java
@@ -1,17 +1,18 @@
 package boundary.rest_interface;
 
+import javax.ws.rs.FormParam;
+
 import exceptions.DALException;
 
 public interface IWeightREST {
 
 	/**
 	 * Initiate a connection to a weight
-	 * @param ip
-	 * @param port
-	 * @return 
+	 * @param chooseWeight
+	 * @return
 	 * @throws DALException
 	 */
-	public String doConnection() throws DALException;
-	
-	
+	public String doConnection(@FormParam("chooseWeight") String chooseWeight) throws DALException;
+
+
 }
