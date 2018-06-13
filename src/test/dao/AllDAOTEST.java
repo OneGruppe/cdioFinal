12e9ec38e928899171d1a93bf956dfa06fb26a4f
Niklaes Jacobsen <niklaes@gmail.com>
2018-06-13 15:15:27 +0200

    smallboychange

---
+++ src/test/dao/AllDAOTEST.java
@@ -0,0 +1,13 @@
+package test.dao;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+
+
+@SuiteClasses({CommodityBatchDAOTEST.class, CommodityDAOTEST.class, ProductBatchComponentDAOTEST.class, ProductBatchDAOTEST.class, RecipeComponentDAOTEST.class, RecipeDAOTEST.class, SupplierDAOTEST.class, UserDAOTEST.class})
+public class AllDAOTEST {
+	
+}
