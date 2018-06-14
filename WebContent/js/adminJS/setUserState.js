45552c0e95ba87401eca0c20c4ee742e3ed6aabc
Sebastian <s170423@student.dtu.dk>
2018-06-14 21:47:29 +0200

    Undefined nu?

--- WebContent/js/adminJS/setUserState.js
+++ WebContent/js/adminJS/setUserState.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 			method : "POST",
 			error: function(xhr) {
 				console.log(xhr.responseText);
-				alert(xhr.status);
+				console.log(xhr.status);
 			},
 			success : function(data) {
 				alert(data.message);
@@ -35,7 +35,7 @@ $(document).ready(function() {
 			method : "POST",
 			error: function(xhr) {
 				console.log(xhr.responseText);
-				console.log(xhr.status);
+				alert.log(xhr.status);
 			},
 			success : function(data) {
 				alert(data.message);
