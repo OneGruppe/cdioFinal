69d02a53e70fbe9257b56787309444b1cef6f9e0
Sebastian <s170423@student.dtu.dk>
2018-06-14 12:13:57 +0200

    JSON

--- WebContent/js/adminJS/createUser.js
+++ WebContent/js/adminJS/createUser.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
 		$.ajax({
 			url: "/rest/user/createUser",
 			data: $('#createUserForm').serialize(),
+			dataType: "json",
 			contenttype: "application/x-ww-form-urlencoded",
 			method: "POST",
 			error: function(xhr) {
