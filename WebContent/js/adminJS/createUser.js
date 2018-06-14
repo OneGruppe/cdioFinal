08e3b79ec6f8c3f4d514c0669c7ffba5e1698d04
Sebastian <s170423@student.dtu.dk>
2018-06-14 11:27:22 +0200

    Hvad sker der?

--- WebContent/js/adminJS/createUser.js
+++ WebContent/js/adminJS/createUser.js
@@ -3,25 +3,15 @@
  */
 
 $(document).ready(function() {
-	var status = document.getElementById("#userStatus");
+
 	$("#creatingUser").click(function() {
-		if($(status).val() < 0) 
-		{
-            $(status).val('0');
-        } 
-		else if($(status).val > 1) 
-		{
-        	$(status).val('1');
-        }
-		
 		$.ajax({
 			url: "/rest/user/createUser",
 			data: $('#createUserForm').serialize(),
 			contenttype: "application/json",
-			method: "POST",
+			method: "PUT",
 			success: function(data) {
 				console.log(data);
-				alert(data);
 			}
 		});
 		return false;
