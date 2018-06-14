93ef8800b6641e6b2806e10fd85468bac9351556
Sebastian <s170423@student.dtu.dk>
2018-06-14 11:23:54 +0200

    Måske fungerer det nu?

--- WebContent/js/adminJS/createUser.js
+++ WebContent/js/adminJS/createUser.js
@@ -3,8 +3,17 @@
  */
 
 $(document).ready(function() {
-
+	var status = document.getElementById("#userStatus");
 	$("#creatingUser").click(function() {
+		if($(status).val() < 0) 
+		{
+            $(status).val('0');
+        } 
+		else if($(status).val > 1) 
+		{
+        	$(status).val('1');
+        }
+		
 		$.ajax({
 			url: "/rest/user/createUser",
 			data: $('#createUserForm').serialize(),
